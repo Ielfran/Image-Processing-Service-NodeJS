f7eb91b7fc8646c524e0cc6ef3f024656bd5f955
Ielfran <gaganraiqpwo@gmail.com>
2025-07-30 23:48:58 +0545

    This is the initial commit

---
+++ models/imageModel.js
@@ -0,0 +1,22 @@
+
+      
+const mongoose = require('mongoose');
+
+const imageSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User',
+  },
+  originalName: { type: String, required: true },
+  storageName: { type: String, required: true }, // The unique name used for storing the file
+  path: { type: String, required: true },
+  mimetype: { type: String, required: true },
+  size: { type: Number, required: true },
+}, { timestamps: true });
+
+const Image = mongoose.model('Image', imageSchema);
+module.exports = Image;
+
+    
+
